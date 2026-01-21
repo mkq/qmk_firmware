69410578fa20ab8406e60dcc295d128cfc8c7cc6
mkq <1584258+mkq@users.noreply.github.com>
2026-01-21 19:27:22 +0100

    [Keymap] ergodox_ez/mkq: replace * with MEH(APP)
    for AutoHotkey used in https://github.com/mkq/mostil

--- keyboards/ergodox_ez/keymaps/mkq/keymap_layers.c
+++ keyboards/ergodox_ez/keymaps/mkq/keymap_layers.c
@@ -1,7 +1,7 @@
 #define _BA_LAYOUT LAYOUT_ergodox_pretty(/*=======================================================================================================================================================================*/\
 /* base -------|-U------------|-I------------|-A------------|-E------------|-O------------|                               |-S------------|-N------------|-R------------|-T------------|-D------------|------------*/\
  KC_F1         ,LT(_FS,KC_F2) ,LT(_FD,KC_F3) ,LT(_AS,KC_F4) ,LT(_AD,KC_F5) ,KC_F6         ,CK_CYLAY        ,KC_SPC        ,KC_F7         ,KC_F8         ,KC_F9         ,KC_F10        ,KC_F11        ,KC_F12        \
-,LT(_LY,DE_CI) ,KC_X          ,KC_V          ,KC_SPC        ,KC_C          ,KC_W          ,KC_Q            ,KC_PGUP       ,KC_K          ,KC_H          ,KC_G          ,KC_F          ,DE_Y          ,DE_ASTR       \
+,LT(_LY,DE_CI) ,KC_X          ,KC_V          ,KC_SPC        ,KC_C          ,KC_W          ,KC_Q            ,KC_PGUP       ,KC_K          ,KC_H          ,KC_G          ,KC_F          ,DE_Y          ,MEH(APP)      \
 ,LT(_L5,CK_SB) ,LWIN_T(KC_U)  ,LALT_T(KC_I)  ,LCTL_T(KC_A)  ,LT(_NV,KC_E)  ,KC_O                                          ,KC_S          ,LT(_NV,KC_N)  ,RCTL_T(KC_R)  ,LALT_T(KC_T)  ,RWIN_T(KC_D)  ,LT(_L5,DE_DQ) \
 ,LT(_L4,DE_PL) ,LT(_DW,DE_MI) ,LT(_DA,CK_QX) ,LT(_DC,KC_L)  ,KC_P          ,DE_Z          ,KC_DEL          ,KC_PGDN       ,KC_B          ,KC_M          ,DE_COMM       ,DE_DOT        ,KC_J          ,TD(SQ_DQ_L4)  \
 ,KC_LWIN       ,KC_LALT       ,KC_LCTL       ,KC_PSCR       ,BSPC                                                                        ,LT(_NV,ESC)   ,KC_UP         ,KC_DOWN       ,KC_LEFT       ,KC_RGHT       \
